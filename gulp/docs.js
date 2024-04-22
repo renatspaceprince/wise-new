1a90e4d3090330047171b32823aa23ae82428c28
renatspaceprince <renatspaceprince@gmail.com>
2024-04-22 13:48:52 +0300

    docs=dev

--- gulp/docs.js
+++ gulp/docs.js
@@ -1,36 +1,23 @@
 const gulp = require('gulp')
-const replace = require('gulp-replace')
-
-// HTML
 const fileInclude = require('gulp-file-include')
-const htmlclean = require('gulp-htmlclean')
-const typograf = require('gulp-typograf')
-
-// SASS
 const sass = require('gulp-sass')(require('sass'))
 const sassGlob = require('gulp-sass-glob')
-const autoprefixer = require('gulp-autoprefixer')
-const csso = require('gulp-csso')
-// const webImagesCSS = require('gulp-web-images-css');  //Вывод WEBP-изображений
-
 const server = require('gulp-server-livereload')
 const clean = require('gulp-clean')
 const fs = require('fs')
 const sourceMaps = require('gulp-sourcemaps')
-const groupMedia = require('gulp-group-css-media-queries')
 const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
 const webpack = require('webpack-stream')
 const babel = require('gulp-babel')
-const changed = require('gulp-changed')
-
-// Images
 const imagemin = require('gulp-imagemin')
+const changed = require('gulp-changed')
+const typograf = require('gulp-typograf')
+const svgsprite = require('gulp-svg-sprite')
+const replace = require('gulp-replace')
 const imageminWebp = require('imagemin-webp')
 const rename = require('gulp-rename')
-
-// SVG
-const svgsprite = require('gulp-svg-sprite')
+const prettier = require('@bdchauvette/gulp-prettier')
 
 gulp.task('clean:docs', function(done) {
   if (fs.existsSync('./docs/')) {
@@ -55,98 +42,86 @@ const plumberNotify = (title) => {
 }
 
 gulp.task('html:docs', function() {
-  return (
-    gulp
-      // .src(['./src/html/**/*.html', '!./src/html/blocks/*.html'])
-      .src([
-        './src/html/**/*.html',
-        '!./**/blocks/**/*.*',
-        '!./src/html/docs/**/*.*',
-      ])
-      .pipe(changed('./docs/'))
-      .pipe(plumber(plumberNotify('HTML')))
-      .pipe(fileInclude(fileIncludeSetting))
-      .pipe(
-        replace(/<img(?:.|\n|\r)*?>/g, function(match) {
-          return match.replace(/\r?\n|\r/g, '').replace(/\s{2,}/g, ' ')
-        })
-      ) //удаляет лишние пробелы и переводы строк внутри тега <img>
-      .pipe(
-        replace(
-          /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
-          '$1./$4$5$7$1'
-        )
+  return gulp
+    .src([
+      './src/html/**/*.html',
+      '!./**/blocks/**/*.*',
+      '!./src/html/docs/**/*.*',
+    ])
+    .pipe(changed('./docs/', { hasChanged: changed.compareContents }))
+    .pipe(plumber(plumberNotify('HTML')))
+    .pipe(fileInclude(fileIncludeSetting))
+    .pipe(
+      replace(/<img(?:.|\n|\r)*?>/g, function(match) {
+        return match.replace(/\r?\n|\r/g, '').replace(/\s{2,}/g, ' ')
+      })
+    ) //удаляет лишние пробелы и переводы строк внутри тега <img>
+    .pipe(
+      replace(
+        /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+        '$1./$4$5$7$1'
       )
-      .pipe(
-        typograf({
-          locale: ['ru', 'en-US'],
-          htmlEntity: { type: 'digit' },
-          safeTags: [
-            ['<\\?php', '\\?>'],
-            ['<no-typography>', '</no-typography>'],
-          ],
-        })
-      )
+    )
+    .pipe(
+      typograf({
+        locale: ['ru', 'en-US'],
+        htmlEntity: { type: 'digit' },
+        safeTags: [
+          ['<\\?php', '\\?>'],
+          ['<no-typography>', '</no-typography>'],
+        ],
+      })
+    )
 
-      .pipe(htmlclean())
-      .pipe(gulp.dest('./docs/'))
-  )
+    .pipe(
+      prettier({
+        tabWidth: 4,
+        useTabs: true,
+        printWidth: 182,
+        trailingComma: 'es5',
+        bracketSpacing: false,
+      })
+    )
+    .pipe(gulp.dest('./docs/'))
 })
 
 gulp.task('sass:docs', function() {
-  return (
-    gulp
-      .src('./src/scss/*.scss')
-      .pipe(changed('./docs/css/'))
-      .pipe(plumber(plumberNotify('SCSS')))
-      .pipe(sourceMaps.init())
-      .pipe(sassGlob()) /* Первый */
-      .pipe(sass()) /* Второй */
-      .pipe(autoprefixer()) /* После SASS обработка CSS */
-      .pipe(groupMedia())
-      // .pipe(
-      // 	webImagesCSS({
-      // 		mode: 'webp',
-      // 	})
-      // )
-      .pipe(
-        replace(
-          /(['"]?)(\.\.\/)+(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
-          '$1$2$3$4$6$1'
-        )
+  return gulp
+    .src('./src/scss/*.scss')
+    .pipe(changed('./docs/css/'))
+    .pipe(plumber(plumberNotify('SCSS')))
+    .pipe(sourceMaps.init())
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(
+      replace(
+        /(['"]?)(\.\.\/)+(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+        '$1$2$3$4$6$1'
       )
-      .pipe(csso())
-      .pipe(sourceMaps.write())
-      .pipe(gulp.dest('./docs/css/'))
-  )
+    )
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('./docs/css/'))
 })
 
 gulp.task('images:docs', function() {
-  return gulp
-    .src(['./src/img/**/*', '!./src/img/svgicons/**/*'])
-    .pipe(changed('./docs/img/'))
-    .pipe(
-      imagemin([
-        imageminWebp({
-          quality: 85,
-        }),
-      ])
-    )
-    .pipe(rename({ extname: '.webp' }))
-    .pipe(gulp.dest('./docs/img/'))
-    .pipe(gulp.src('./src/img/**/*'))
-    .pipe(changed('./docs/img/'))
-    .pipe(
-      imagemin(
-        [
-          imagemin.gifsicle({ interlaced: true }),
-          imagemin.mozjpeg({ quality: 85, progressive: true }),
-          imagemin.optipng({ optimizationLevel: 5 }),
-        ],
-        { verbose: true }
+  return (
+    gulp
+      .src(['./src/img/**/*', '!./src/img/svgicons/**/*'])
+      .pipe(changed('./docs/img/'))
+      .pipe(
+        imagemin([
+          imageminWebp({
+            quality: 85,
+          }),
+        ])
       )
-    )
-    .pipe(gulp.dest('./docs/img/'))
+      .pipe(rename({ extname: '.webp' }))
+      .pipe(gulp.dest('./docs/img/'))
+      .pipe(gulp.src(['./src/img/**/*', '!./src/img/svgicons/**/*']))
+      .pipe(changed('./docs/img/'))
+      // .pipe(imagemin({ verbose: true }))
+      .pipe(gulp.dest('./docs/img/'))
+  )
 })
 
 const svgStack = {
@@ -154,6 +129,15 @@ const svgStack = {
     stack: {
       example: true,
     },
+  },
+  shape: {
+    transform: [
+      {
+        svgo: {
+          js2svg: { indent: 4, pretty: true },
+        },
+      },
+    ],
   },
 }
 
@@ -167,6 +151,7 @@ const svgSymbol = {
     transform: [
       {
         svgo: {
+          js2svg: { indent: 4, pretty: true },
           plugins: [
             {
               name: 'removeAttrs',
@@ -184,7 +169,7 @@ const svgSymbol = {
 gulp.task('svgStack:docs', function() {
   return gulp
     .src('./src/img/svgicons/**/*.svg')
-    .pipe(plumber(plumberNotify('SVG:dev')))
+    .pipe(plumber(plumberNotify('SVG:docs')))
     .pipe(svgsprite(svgStack))
     .pipe(gulp.dest('./docs/img/svgsprite/'))
 })
@@ -192,7 +177,7 @@ gulp.task('svgStack:docs', function() {
 gulp.task('svgSymbol:docs', function() {
   return gulp
     .src('./src/img/svgicons/**/*.svg')
-    .pipe(plumber(plumberNotify('SVG:dev')))
+    .pipe(plumber(plumberNotify('SVG:docs')))
     .pipe(svgsprite(svgSymbol))
     .pipe(gulp.dest('./docs/img/svgsprite/'))
 })
@@ -205,13 +190,15 @@ gulp.task('files:docs', function() {
 })
 
 gulp.task('js:docs', function() {
-  return gulp
-    .src('./src/js/*.js')
-    .pipe(changed('./docs/js/'))
-    .pipe(plumber(plumberNotify('JS')))
-    .pipe(babel())
-    .pipe(webpack(require('./../webpack.config.js')))
-    .pipe(gulp.dest('./docs/js/'))
+  return (
+    gulp
+      .src('../src/js/*.js')
+      .pipe(changed('./docs/js/'))
+      .pipe(plumber(plumberNotify('JS')))
+      // .pipe(babel())
+      .pipe(webpack(require('./../webpack.config.js')))
+      .pipe(gulp.dest('./docs/js/'))
+  )
 })
 
 const serverOptions = {
@@ -221,4 +208,19 @@ const serverOptions = {
 
 gulp.task('server:docs', function() {
   return gulp.src('./docs/').pipe(server(serverOptions))
+})
+
+gulp.task('watch:docs', function() {
+  gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass:docs'))
+  gulp.watch(
+    ['./src/html/**/*.html', './src/html/**/*.json'],
+    gulp.parallel('html:docs')
+  )
+  gulp.watch('./src/img/**/*', gulp.parallel('images:docs'))
+  gulp.watch('./src/files/**/*', gulp.parallel('files:docs'))
+  gulp.watch('./src/js/**/*.js', gulp.parallel('js:docs'))
+  gulp.watch(
+    './src/img/svgicons/*',
+    gulp.series('svgStack:docs', 'svgSymbol:docs')
+  )
 })
